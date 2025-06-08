b0ab7a6557ade8a41f47b552b1fade9ca00f62f9
Krish Sinha <krishsinha0304@gmail.com>
2025-06-08 15:23:52 +0530

    implement home page layout & companion builder layout

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanionId = () => {
+  return (
+    <div>Companion Id</div>
+  )
+}
+
+export default CompanionId
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <>
-    <h1 className='text-2xl underline '>Welcome to my Saas</h1>
-    <Button>Shadcn Button</Button>
-    </>
+    <main>
+    <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name='Neura the Brainy Explorer'
+          topic='Neural Network of the brain'
+          subject='science'
+          duration={45}
+          color='#ffda6e'
+        />
+        <CompanionCard
+          id='456'
+          name='Countsy the number wizard'
+          topic='Derivatives and Integrals'
+          subject='maths'
+          duration={30}
+          color='#e5d0ff'
+        />
+        <CompanionCard
+          id='786'
+          name='Verba the vocabulary builder'
+          topic='English Literature'
+          subject='language'
+          duration={30}
+          color='#BDE7FF'
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionList
+          title='Recently completed sessions'
+          companions={recentSessions}
+          className='w-2/3 max-lg:w-full'
+        />
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
